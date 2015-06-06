806842ef11a9618d9c836db6923113095e05b515
Preston Pham <prestonp08@gmail.com>
2015-06-06 18:59:18 -0500

    fix

--- js/occupylege.js
+++ js/occupylege.js
@@ -1,3 +1,19 @@
-/**
- * 
- */
+function updateOccupyResults(data) {
+  var prefix = data.chamber.indexOf('Senate') < 0 ? 'Representative' : 'Senator';
+  var name = prefix + ' ' + data.name;
+
+  var $welcomeRepName = $('#welcomeRepName');
+  $welcomeRepName.html(name);
+
+  var $callRepButton = $('#callRepButton');
+  var phone = data.districtPhone || data.capitalPhone || '';
+  $callRepButton.attr('href', 'tel:' + phone.replace(/[-()\s]/g, ''));
+  $callRepButton.html('Call ' + data.name + ' Now!');
+
+  var $emailRepName = $('#emailRepName');
+  $emailRepName.html(name);
+
+  var $emailButton = $('#emailButton');
+  $emailButton.html('Email ' + name + ' Now!');
+  $emailButton.attr('href', 'mailto:' + data.email);
+}
